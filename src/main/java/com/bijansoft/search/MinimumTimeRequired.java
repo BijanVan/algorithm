a5f2b68a598bc5119567bd46a2ecc7097a30a7dd
Bijan <bijannazem@gmail.com>
2020-03-17 11:11:35 -0700

    added some search algorithms

--- src/main/java/com/bijansoft/search/MinimumTimeRequired.java
+++ src/main/java/com/bijansoft/search/MinimumTimeRequired.java
@@ -1,7 +1,45 @@
 package com.bijansoft.search;
 
+import java.util.Arrays;
+
 public class MinimumTimeRequired {
     static long minTime(long[] machines, long goal) {
-        throw new UnsupportedOperationException();
+        long start = shortestMinTime(machines, goal);
+        long end = longestMinTime(machines, goal);
+
+        Arrays.sort(machines);
+
+        while (end - start > 1) {
+            long middle = start + (end - start) / 2;
+            long productCount = calculateProduction(machines, middle);
+            if (productCount >= goal) {
+                end = middle;
+            } else {
+                start = middle;
+            }
+        }
+
+        if (calculateProduction(machines, start) == calculateProduction(machines, end))
+            return start;
+        else
+            return end;
+    }
+
+    private static long longestMinTime(long[] machines, long goal) {
+        return machines[0] * goal;
+    }
+
+    private static long shortestMinTime(long[] machines, long goal) {
+        double total = 0;
+        for (long machine : machines)
+            total += 1.0 / machine;
+        return (long) (goal / total);
+    }
+
+    private static long calculateProduction(long[] machines, long days) {
+        long productCount = 0;
+        for (long machine : machines)
+            productCount += days / machine;
+        return productCount;
     }
 }
